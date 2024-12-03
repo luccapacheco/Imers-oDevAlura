2da49becf9325a8143e01fd01de221c1e15d4c56
luccapacheco <42037982+luccapacheco@users.noreply.github.com>
2024-12-03 10:17:13 -0300

    Delete src directory

--- src/utils/responseHandler.js
+++
@@ -1,8 +0,0 @@
-// Função para enviar respostas padronizadas
-export function sendResponse(res, status, message, data = null) {
-    return res.status(status).json({
-      message,
-      data,
-    });
-  }
-  
